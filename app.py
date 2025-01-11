e972d975a3592981b54bef0e5464150606271c2d
pearhoang <35418720+pearhoang@users.noreply.github.com>
2025-01-11 16:42:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 import sys
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
-os.environ['GRADIO_ANALYTICS_ENABLED'] = '0'
+
 sys.path.insert(0, os.getcwd())
-sys.path.append(os.path.join(os.path.dirname(__file__), 'sd-scripts'))
+
 import subprocess
 import gradio as gr
 from PIL import Image
